79337f451bc81a184bfe0f738eee1a26b2b99c2d
iad640 <ivan.alonso@uib.es>
2021-12-22 02:08:25 +0100

    New stuff

--- 4_Data_importing_and_wrangling.R
+++ 4_Data_importing_and_wrangling.R
@@ -27,7 +27,7 @@ wd<-getwd()
 #If the path does not start with /, R interprets that this is a relative path. If the path starts with /, it is interpreted as a full path from the root directory
 setwd(dir = "projects/R_Fundamentals/")
 
-#------------------COPYING FILES------------------------------------
+#------------------1.1.-COPYING FILES------------------------------------
 #It is always important to copy files to the working directory, to have always relative paths
 #We do this with the file.copy function. This function copies the file from the path in the first argument
 #to our home directory with the name given in the second argument
@@ -166,4 +166,117 @@ dat%>%separate(key, c("year", "first_variable_name","second_variable_name"),fill
 dat%>%separate(key, c("year", "first_variable_name","second_variable_name"),fill="right")%>%unite(variable_name,first_variable_name, second_variable_name)%>%spread(variable_name,value)
 dat%>%separate(key, c("year", "first_variable_name","second_variable_name"),fill="right")%>%unite(variable_name,first_variable_name, second_variable_name)%>%spread(variable_name,value)%>%rename(fertility = fertility_NA)
 
-                                                                                                                                                                                                  
+
+#---------------------3.- JOINING TABLES----------------------
+
+#The information we need for a given analysis may not be just in one table. Suppose we want to explore the relationship
+# between population size for US states and electoral votes. We have population size in this table
+library(tidyverse)
+library(dslabs)
+data(murders)
+#and electoral votes here
+data(polls_us_election_2016)
+head(murders)
+head(results_us_election_2016)
+#Concatenating these tables will not work since the order of the sates is not the same
+identical(results_us_election_2016$state,murders$state)
+#The join functions in the dplyr package make sure that the tables are combined so that matching rows are together. 
+# The general idea is that one needs to identify one or more columns that will serve to match the two tables. Then 
+#a new table with the combined information is returned. Notice what happens if we join the two tables above by state 
+#using left_join (we will remove the others column and rename electoral_votes so that the tables fit on the page):
+tab<-left_join(murders, results_us_election_2016, by ="state")%>%select(-others)%>%rename(ev=electoral_votes)
+head(tab)
+#The data has been successfully joined and we can now, for example, make a plot to explore the relationship:
+library(ggrepel)
+tab %>% ggplot(aes(population/10^6, ev, label = abb)) + geom_point() + geom_text_repel() + scale_x_continuous(trans="log2") + scale_y_continuous(trans="log2") + geom_smooth(method="lm", se = FALSE)
+
+#In practice, it is not always the case that each row in one table has a matching row in the other. For this reason, 
+#we have several versions of join. To illustrate this challenge, we will take subsets of the tables above. We create
+#the tables tab1 and tab2 so that they have some states in common but not all:
+tab1<- slice (murders, 1:6) %>% select (state, population)
+tab1
+tab2<-results_us_election_2016%>%filter(state %in% c("Alabama", "Alaska", "Arizona", "California", "Connecticut", "Delaware" ))%>%select(state, electoral_votes)%>%rename(ev=electoral_votes)
+tab2
+#------------------Left join----------------------------
+#Suppose we want a table like tab1 adding electoral votes when available. Then we use left join, with tab1 being the first argument
+#We choose which column to use to match with the by argument
+left_join(tab1, tab2, by ="state")
+#Note that NAs are added to the states from tab1 not appearing in tab2
+#---------------------Right join-------------------------
+#If we want a table with the same rows as a second table
+tab1%>%right_join(tab2, by ="state")
+#--------------Inner join-----------------------
+#If we want to keep only the rows that have information in both tables. Think of it as an intersection
+inner_join(tab1, tab2, by ="state")
+#--------------Full join-------------------
+#If we want to keep all the rows and fill the missing parts with NAs. Think of it as a Union
+full_join(tab1, tab2, by ="state")
+#----------------------Semi join---------------------
+#The semi_join function lets us keep the part of first table for which we have information in the second. 
+#It does not add the columns of the second:
+semi_join(tab1, tab2, by="state")
+#------------------------anti join---------------------
+#The function anti_join is the opposite of semi_join. It keeps the elements of the first table for 
+#which there is no information in the second:
+anti_join(tab1, tab2, by ="state")
+
+#--------------------BINDING------------------------------
+#Although we have yet to use it in this book, another common way in which datasets are combined is 
+#by binding them. Unlike the join function, the binding functions do not try to match by a variable, but instead 
+#simply combine datasets. If the datasets don’t match by the appropriate dimensions, one obtains an error
+#The dplyr function bind_cols binds two objects by making them columns in a tibble. For example, we quickly want
+#to make a data frame consisting of numbers we can use.
+bind_cols(a=1:3, b=4:6)
+#This function requires that we assign names to the columns. Here we chose a and b.
+#Note that there is an R-base function cbind with the exact same functionality. An important difference is that 
+#cbind can create different types of objects, while bind_cols always produces a data frame.
+
+#bind_cols can also bind two different data frames. For example, here we break up the tab data frame and then bind 
+#them back together:
+tab_1<-tab[,1:3]
+tab_2<-tab[4:6]
+tab_3<-tab[,7:8]
+new_tab<-bind_cols(tab_1, tab_2, tab_3)
+head(new_tab)
+
+#The bind_rows function is similar to bind_cols, but binds rows instead of columns:
+tab_1<-tab[1:2,]
+tab_2<-tab[3:4,]
+bind_rows(tab_1, tab_2)
+
+#---------------------------Set operators----------------------------------
+#Another set of commands useful for combining datasets are the set operators. When applied to vectors, these behave 
+#as their names suggest. Examples are intersect, union, setdiff, and setequal. However, if the tidyverse, or more 
+# specifically dplyr, is loaded, these functions can be used on data frames as opposed to just on vectors.
+intersect(1:10, 6:15)
+intersect(c("a","b","c"),c("b","c","d"))
+#The dplyr package includes an intersect function that can be applied to tables with the same column names. This 
+#function returns the rows in common between two tables. To make sure we use the dplyr version of intersect rather 
+#than the base package version, we can use dplyr::intersect like this:
+tab_1<-tab[1:5,]
+tab_1
+tab_2<-tab[3:7,]
+tab_2
+dplyr::intersect(tab_1,tab_2)
+#Similarly union takes the union of vectors. For example:
+union(1:10, 6:15)
+union(c("a","b","c"),c("c","d", "e"))
+#The dplyr package includes a version of union that combines all the rows of two tables with the same column names.
+tab_1<-tab[1:5,]
+tab_2<-tab[3:7,]
+dplyr::union(tab_1, tab_2)
+#The set difference between a first and second argument can be obtained with setdiff. Unlike intersect and union, 
+#this function is not symmetric:
+setdiff(1:10, 6:15)
+setdiff(6:15, 1:10)
+#As for the functions above, dplyr has a function for data frames
+tab_1<-tab[1:5,]
+tab_2<-tab[3:7,]
+dplyr::setdiff(tab_1,tab_2)
+#Finally, the function setequal tells us if two sets are the same, regardless of order. So notice that:
+setequal(1:5,1:6)
+#but
+setequal(1:5, 5:1)
+#When applied to data frames that are not equal, regardless of order, the dplyr version provides a useful 
+#message letting us know how the sets are different:
+dplyr::setequal(tab_1, tab_2)
--- 4_Data_importing_and_wrangling_exercises.R
+++ 4_Data_importing_and_wrangling_exercises.R
@@ -23,3 +23,63 @@ admissions<-gather(admissions,key, value,-major,-gender)%>%unite(column_name, ge
 #Add two columns with the percentages
 admissions<-admissions%>%mutate(percentage_admitted_men=100*men_admitted/men_applicants, percentage_admitted_women=100*women_admitted/women_applicants)
 admissions
+
+#Joining table exercises, from 22.4 section of Introduction to data science
+#load the Lahman library. This database includes data related to baseball teams.
+#It includes summary statistics about how the players performed on offense and defense for several years. It also 
+#includes personal information about the players.
+library(Lahman)
+
+#The Batting data frame contains the offensive statistics for all players for many years. You can see, for example, 
+#the top 10 hitters by running this code:
+Batting
+top<-Batting%>%filter(yearID==2016)%>%arrange(desc(HR))%>%slice(1:10)
+top%>%as_tibble()
+#But who are these players? We see an ID, but not the names. The player names are in this table
+Master%>%as_tibble()
+#We can see column names nameFirst and nameLast. Use the left_join function to create a table of the top home run 
+#hitters. The table should have playerID, first name, last name, and number of home runs (HR). Rewrite the object 
+#top with this new table.
+top<-top%>%left_join(Master)%>%select("playerID","nameFirst","nameLast","HR")
+top
+
+#Now use the Salaries data frame to add each player’s salary to the table you created in the previous exercise. Note 
+#that salaries are different every year so make sure to filter for the year 2016, then use right_join. This time show 
+#first name, last name, team, HR, and salary.
+Salaries
+top<-top%>%right_join(Salaries)%>%filter(yearID==2016)%>%select("nameFirst","nameLast","teamID","HR","salary")%>%slice(1:10)
+top
+
+#In a previous exercise, we created a tidy version of the co2 dataset:
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) %>% 
+  setNames(1:12) %>%
+  mutate(year = 1959:1997) %>%
+  pivot_longer(-year, names_to = "month", values_to = "co2") %>%
+  mutate(month = as.numeric(month))
+co2_wide
+#We want to see if the monthly trend is changing so we are going to remove the year effects and then plot the results.
+#We will first compute the year averages. Use the group_by and summarize to compute the average co2 for each year.
+#Save in an object called yearly_avg.
+yearly_avg<-co2_wide%>%group_by(year)%>%summarize(avg=mean(co2))
+yearly_avg
+#Now use the left_join function to add the yearly average to the co2_wide dataset. Then compute the residuals: observed 
+#co2 measure - yearly average.
+
+co2_wide<-co2_wide%>%left_join(yearly_avg, by ="year")%>%mutate(residuals = co2 - avg)
+co2_wide_year<-co2_wide%>%select(year,month,co2,residuals)%>%group_by(month)
+
+co2_wide<-co2_wide%>%select(co2,residuals)
+co2_wide
+#Make a plot of the seasonal trends by year but only after removing the year effect.
+#I think he means to do this: https://anomaly.io/seasonal-trend-decomposition-in-r/index.html
+
+library(forecast) #We load the package to work with time series
+#To detect the underlying trend, we smooth the time series using the “centred moving average“. To perform the 
+#decomposition, it is vital to use a moving window of the exact size of the seasonality. Therefore, to decompose 
+#a time series we need to know the seasonality period: weekly, monthly, etc… If you don’t know this figure, you 
+#can detect the seasonality using a Fourier transform.
+co2_ts<-ts(co2_wide, frequency=12, start=c(1959,1))
+plot.ts(co2_ts) #The first plot represents the seasonal trend including the yearly effect. The second one does not have
+#into account the yearly effect
+
+
